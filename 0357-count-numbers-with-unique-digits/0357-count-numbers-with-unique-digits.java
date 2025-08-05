01b2c67940ddda33e40bcc4a1c0771c1cbd67c37
akshika-6 <akshika27ak@gmail.com>
2025-08-05 23:12:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        int limit = (int) Math.pow(10,n);
+        int count = 0;
+        for(int i=0;i<limit;i++){
+            int cnt=0;
+            int temp = i;
+            HashSet<Integer> hs = new HashSet<>();
+            boolean isUnique = true;
+
+            // Special case for 0
+            if (i == 0) {
+                count++;
+                continue;
+            }
+            while (temp > 0) {
+                int d = temp % 10;
+                if (hs.contains(d)) {
+                    isUnique = false;
+                    break;
+                }
+                hs.add(d);
+                temp = temp / 10;
+            }
+
+            if (isUnique) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

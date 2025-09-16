3f8814029567e3f4045a53bd74032e689b5af77d
akshika-6 <akshika27ak@gmail.com>
2025-09-16 16:10:53 +0530

    Time: 0 ms (100%), Space: 43.3 MB (52.35%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,31 +1,43 @@
+class UnionFind{
+    int parent[];
+    int rank[];
+    UnionFind(int V){
+        parent = new int[V+1];
+        rank = new int[V+1];
+        for(int i=0;i<V;i++){
+            parent[i]=i;
+        }
+    }
+    int find(int node){
+        if(parent[node] == node) return node;
+        return find(parent[node]);
+    }
+    boolean union(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu == pv){
+            return false;
+        }
+        if(rank[pu] > rank[pv]){
+            parent[pv]=pu;
+        } else if(rank[pv] > rank[pu]){
+            parent[pu]=pv;
+        } else{
+            parent[pv]=pu;
+            pu++;
+        }
+        return true;
+    }
+}
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        int n = edges.length;
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for(int[] edge:edges){
-            int u=edge[0],v=edge[1];
-            boolean[] visited=new boolean[n+1];
-            if(hasPath(u,v,graph,visited)){
-                return edge;
+        int n=edges.length;
+        UnionFind uf=new UnionFind(n);
+        for(int[] i:edges){
+            if(uf.union(i[0],i[1]) == false){
+                return i;
             }
-            graph.get(u).add(v);
-            graph.get(v).add(u);
         }
         return new int[0];
-    }
-    public static boolean hasPath(int u,int v,List<List<Integer>> graph,boolean[] visited){
-        if(u==v){
-            return true;
-        }
-        visited[u]=true;
-        for(int nei:graph.get(u)){
-            if(!visited[nei] && hasPath(nei,v,graph,visited)){
-                return true;
-            }
-        }
-        return false;
     }
 }

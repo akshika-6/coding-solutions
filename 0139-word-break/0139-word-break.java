d8cdb3837f359d26ca707a5c4db4ed17696d9c1f
akshika-6 <akshika27ak@gmail.com>
2025-10-10 12:02:52 +0530

    Time: 8 ms (42.47%), Space: 44.9 MB (20.87%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> hs=new HashSet<>(wordDict);
-        boolean[] dp=new boolean[s.length()+1];
-        dp[0]=true;
-
-        for(int i = 1;i<=s.length();i++){
-            for(int j=0;j<i;j++){
-                if(dp[j] && hs.contains(s.substring(j,i))){
-                    dp[i]=true;
-                    break;
+        Map<String,Boolean> memo=new HashMap<>();
+        return helper(s,memo,hs);
+    }
+    public static boolean helper(String s,Map<String,Boolean> memo,Set<String> hs){
+        if(s.isEmpty()) return true;
+        if(memo.containsKey(s)) return memo.get(s);
+        for(int i=1;i<=s.length();i++){
+            String prefix=s.substring(0,i);
+            if(hs.contains(prefix)){
+                String suffix=s.substring(i);
+                if(helper(suffix,memo,hs)){
+                    memo.put(s,true);
+                    return true;
                 }
             }
         }
-        return dp[s.length()];
+        memo.put(s,false);
+        return false;
     }
 }

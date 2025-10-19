a474f9696d4636013833d14a836d6c82030f1a3c
akshika-6 <akshika27ak@gmail.com>
2025-10-19 12:29:24 +0530

    Time: 64 ms (77.53%), Space: 48 MB (60.67%) - LeetHub

---
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String findLexSmallestString(String s, int a, int b) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        
+        String smallest = s;
+        queue.offer(s);
+        visited.add(s);
+        
+        while (!queue.isEmpty()) {
+            String cur = queue.poll();
+            if (cur.compareTo(smallest) < 0) {
+                smallest = cur;
+            }
+
+            // Operation 1: Add 'a' to all digits at odd indices
+            char[] chars = cur.toCharArray();
+            for (int i = 1; i < chars.length; i += 2) {
+                chars[i] = (char) ((chars[i] - '0' + a) % 10 + '0');
+            }
+            String added = new String(chars);
+            if (visited.add(added)) {
+                queue.offer(added);
+            }
+
+            // Operation 2: Rotate right by b
+            String rotated = cur.substring(cur.length() - b) + cur.substring(0, cur.length() - b);
+            if (visited.add(rotated)) {
+                queue.offer(rotated);
+            }
+        }
+        
+        return smallest;
+    }
+}

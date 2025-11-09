51acd1633eea2387497e035033f68d21a12717b5
akshika-6 <akshika27ak@gmail.com>
2025-11-09 13:11:25 +0530

    Time: 58 ms (21.72%), Space: 50.5 MB (36.79%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n = stones.length;
+       return solveUtil(n,stones);
+    }
+    static boolean solveUtil(int n, int[] stones) {
+        Map<Integer, Set<Integer>> dp = new HashMap<>();
+        for (int stone : stones) {
+            dp.put(stone, new HashSet<>());
+        }
+
+        dp.get(stones[0]).add(0); // Start at first stone with jump size 0
+
+        for (int i = 0; i < n; i++) {
+            int position = stones[i];
+            for (int jump : dp.get(position)) {
+                for (int step = jump - 1; step <= jump + 1; step++) {
+                    if (step > 0) {
+                        int nextPos = position + step;
+                        if (nextPos == stones[n - 1]) return true; // reached last stone
+                        if (dp.containsKey(nextPos)) {
+                            dp.get(nextPos).add(step);
+                        }
+                    }
+                }
+            }
+        }
+
+        return !dp.get(stones[n - 1]).isEmpty();
+    }
+}

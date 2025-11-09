0285c6fd2460519a3b789e92ccc50cd1bc2f665a
akshika-6 <akshika27ak@gmail.com>
2025-11-09 15:44:34 +0530

    Time: 2 ms (99.37%), Space: 83.5 MB (5.91%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.java
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -4,8 +4,9 @@ class Solution {
         if (n == 2) return stones[1] - stones[0];
 
         int ans = 0;
-        for (int i = 0; i + 2 < n; i++) {
-            ans = Math.max(ans, stones[i + 2] - stones[i]);
+        for (int i = 2; i < n; i++) {
+            int gap = stones[i] - stones[i - 2];
+            if (gap > ans) ans = gap;
         }
         return ans;
     }

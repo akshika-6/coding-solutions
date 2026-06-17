37f3727c59786aedeeeb930aa393139dd5b342ad
Akshika Gupta <akshika27ak@gmail.com>
2026-06-17 20:56:45 +0530

    Time: 152 ms (5.01%), Space: 47.6 MB (21.26%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int num:nums1){
+            set1.add(num);
+        }
+        for(int num:nums2){
+            set2.add(num);
+        }
+
+        List<Integer> res1 = new ArrayList<>();
+        List<Integer> res2 = new ArrayList<>();
+        for(int i:set1){
+            if(!set2.contains(i)){
+                res1.add(i);
+            }
+        }
+        for(int i:set2){
+            if(!set1.contains(i)){
+                res2.add(i);
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(res1);
+        res.add(res2);
+
+        return res;
+    }
+}

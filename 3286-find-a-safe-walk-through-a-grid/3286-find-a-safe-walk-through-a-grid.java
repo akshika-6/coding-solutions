c978319c435c3c344e10c7b74d297a6300b036d2
Akshika Gupta <akshika27ak@gmail.com>
2026-07-02 22:48:19 +0530

    Time: 12 ms (80.74%), Space: 46.9 MB (70.49%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        int m = grid.size();
+        int n = grid.get(0).size();
+
+        int[][] dist = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+
+        Deque<int[]> dq = new ArrayDeque<>();
+
+        dist[0][0] = grid.get(0).get(0);
+        dq.offerFirst(new int[]{0, 0});
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while (!dq.isEmpty()) {
+            int[] cur = dq.pollFirst();
+            int r = cur[0];
+            int c = cur[1];
+
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr < 0 || nc < 0 || nr >= m || nc >= n)
+                    continue;
+
+                int newCost = dist[r][c] + grid.get(nr).get(nc);
+
+                if (newCost < dist[nr][nc]) {
+                    dist[nr][nc] = newCost;
+
+                    if (grid.get(nr).get(nc) == 0)
+                        dq.offerFirst(new int[]{nr, nc});
+                    else
+                        dq.offerLast(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return dist[m - 1][n - 1] < health;
+    }
+}
